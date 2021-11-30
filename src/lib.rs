d3560b16913f2454195aea21e2c53fb44966bb81
ByteAlex <bytealex@zerotwo.bot>
2021-11-30 20:15:03 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
-mod builder;
 pub mod internal;
 
-pub use crate::builder::QueryBuilder;
 /// Re-Exports
 pub use smartql_macro::{args, smartql_init, smartql_init_lazy, smartql_object, SmartQlObject};
 pub use async_trait::async_trait;
-
-use async_trait::async_trait;
 
 #[async_trait]
 pub trait SmartQlObject {
@@ -16,8 +12,6 @@ pub trait SmartQlObject {
     ) -> sqlx::Result<Option<Self>>
     where
         Self: Sized;
-
-    //fn query<B: QueryBuilder<<Self as SmartQlObject>::SelfType>>() -> B;
 
     async fn save_all(&mut self, executor: &sqlx::Pool<sqlx::MySql>) -> sqlx::Result<bool>;
 
